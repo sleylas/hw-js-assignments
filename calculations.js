8fdf52a9cf4d3835de3653b6c44a7ac0a7ceb4f9
sleylas <119806173+sleylas@users.noreply.github.com>
2023-02-10 02:24:50 +0400

    Corrections

--- calculations.js
+++ calculations.js
@@ -1,25 +1,27 @@
+
+Corrections
 function increaseBalance(x, y) {
-  x = x + y;
-  return x;
+    x = x + y;
+    return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+    x = x - y;
+    return x;
 }
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
-  return x;
-  console.log("x");
+    x = x / y;
+    console.log("x")
+    return x;
 }
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
-  return x;
+    x = x % y;
+    return x;
 }
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 700));
 console.log(getRestAfterDivision(7000, 3));
